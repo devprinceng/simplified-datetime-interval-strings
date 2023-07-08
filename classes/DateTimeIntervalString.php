c9eb3e4e7f75232ba8f4a1878cf58feff3479707
devprinceng <devprinceng@gmail.com>
2023-07-08 07:06:22 +0100

    DateTimeIntervalString class completed

---
+++ classes/DateTimeIntervalString.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * DateTimeIntervalString - a simplied Carbon alternative for PHP.
+ * Handles datetime interval strings such as '0 sec ago', '1 mins ago', '2 hours ago', '2 Days ago','months ago', years ago.
+ * 
+ * Example:
+ *  $datetime_string = "2023-07-06 12:20:28"  
+ *  echo DateTimeStringInterval::get_interval($datetime_string); //5 hours ago
+ * 
+ * @author PRINCE OJOI <devprinceng@gmail.com>  
+ * @copyright (c) 2023 Prince Ojoi
+ * @link https://github.com/devprinceng/simplified-datetime-interval-strings
+ * @license MIT License
+ */
+class DateTimeIntervalString
+{    
+    public static string $timezone; 
+    
+    /**
+     * sets default timezone across the whole class
+     * Usage: DateTimeStringInterval::setTimeZone('Africa/Lagos') 
+     * @param string $timezone timezone selected; default: "Africa/Lagos"
+     * @return  void
+     */
+    public static function setTimezone(string $timezone = "Africa/Lagos")
+    {
+        self::$timezone = $timezone;
+        date_default_timezone_set(self::$timezone);
+    }
+
+    /**
+     * returns datetime interval strings from old-date to current date; such as '0 sec ago', '1 mins ago', '2 hours ago', '2 Days ago','months ago', years ago.
+     * 
+     * @param string $datetime_string the old datetime string, eg: '2023-07-06 08:02:05'
+     * @return string 
+     */
+    public static function get_interval(string $datetime_string): string
+    {  
+        $datetime = new DateTime($datetime_string);
+        $current_datetime = new DateTime('now');
+        $current_datetime = new DateTime($current_datetime->format("Y-m-d H:i:s"));
+        $datetime_interval = $datetime->diff($current_datetime);
+        //intervals
+        $year = $datetime_interval->y; 
+        $month = $datetime_interval->m;
+        $day = $datetime_interval->d;
+        $hour = $datetime_interval->h;
+        $minute = $datetime_interval->i;
+        $seconds = $datetime_interval->s;
+       
+        if (!$year) {
+          $interval = $month <= 1 ? "$month mon, $day days ago" : "$month mon, $day days ago";
+            if (!$month) {
+                $interval = $day <= 1 ? "$day day ago" : "$day days ago";
+                if (!$day) {
+                    $interval = $hour <= 1 ? "$hour hr ago" : "$hour hrs ago";
+                    if (!$hour) {
+                        $interval = $minute <= 1 ? "$minute min ago" : "$minute mins ago";
+                        if (!$minute) {
+                            $interval = $seconds <= 1 ? "$seconds sec ago" : "$seconds secs ago";
+                        }
+                    }
+                }
+            }
+        } else {
+            $interval = "$year years ago";
+        }
+
+        return $interval;
+    }
+}
